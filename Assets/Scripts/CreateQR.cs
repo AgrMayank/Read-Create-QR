c78f113ec8137423f4730c26ea93e26e742e9f3c
Mayank Raj <myid.mayank@gmail.com>
2021-05-28 19:09:05 +0530

    Add Create QR UI

--- Assets/Scripts/CreateQR.cs
+++ Assets/Scripts/CreateQR.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using ZXing;
 using ZXing.QrCode;
+using TMPro;
 
 public class CreateQR : MonoBehaviour
 {
+    public string m_TextToEncode = "Write Your QR Code Message Here";
+
+    public TMP_Text m_TextInQR;
+
+    public RawImage m_QRCode;
+
+    private void Start()
+    {
+        CreateQRCode();
+    }
+
     private static Color32[] Encode(string textForEncoding, int width, int height)
     {
         var writer = new BarcodeWriter
@@ -18,21 +31,26 @@ public class CreateQR : MonoBehaviour
                 Width = width
             }
         };
+
         return writer.Write(textForEncoding);
     }
 
-    public Texture2D generateQR(string text)
+    private Texture2D generateQR(string text)
     {
         var encoded = new Texture2D(256, 256);
         var color32 = Encode(text, encoded.width, encoded.height);
+
         encoded.SetPixels32(color32);
         encoded.Apply();
+
         return encoded;
     }
 
-    private void OnGUI()
+    public void CreateQRCode()
     {
-        Texture2D myQR = generateQR("test");
-        if (GUI.Button(new Rect(300, 300, 256, 256), myQR, GUIStyle.none)) { }
+        Texture2D myQR = generateQR(m_TextToEncode);
+        m_QRCode.texture = myQR;
+
+        m_TextInQR.text = "<u>Text in QR:</u>\n\n" + m_TextToEncode;
     }
 }
