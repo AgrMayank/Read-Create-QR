93eb17c88ca835e1c263c07d5346e92796d63e11
Mayank Raj <myid.mayank@gmail.com>
2021-05-28 18:22:06 +0530

    Create Basic QR Read, Write Scenes

---
+++ Assets/Scripts/CreateQR.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using ZXing;
+using ZXing.QrCode;
+
+public class CreateQR : MonoBehaviour
+{
+    private static Color32[] Encode(string textForEncoding, int width, int height)
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+
+    public Texture2D generateQR(string text)
+    {
+        var encoded = new Texture2D(256, 256);
+        var color32 = Encode(text, encoded.width, encoded.height);
+        encoded.SetPixels32(color32);
+        encoded.Apply();
+        return encoded;
+    }
+
+    private void OnGUI()
+    {
+        Texture2D myQR = generateQR("test");
+        if (GUI.Button(new Rect(300, 300, 256, 256), myQR, GUIStyle.none)) { }
+    }
+}
